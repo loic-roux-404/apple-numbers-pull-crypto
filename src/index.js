ef1ad82b05f2f02ebb10abe35130f71b55bf8c04
loic-roux <69loloro10@gmail.com>
2021-06-07 19:34:17 +0200

    syntax: space

--- src/index.js
+++ src/index.js
@@ -3,22 +3,21 @@ import cmcApi from './cmcApi'
 
 ((options) => {
     'use strict';
-		
+
     const app = Application('Numbers')
     app.includeStandardAdditions = true
-    
+ 
     const table = app.documents()[0].sheets()[0].tables()[0]
     const cellsRange = table.cellRange()
-    
-    
+
     const quotes = cmcApi.getQuotes(
         cellsRange
         .cells()
         .flatMap(
             cell => cell.column().address() === 1 ? cell.value() : []
         )
-    ) 
-            
+    )
+
     let tmp = {}
     cellsRange.cells().forEach(cell => {
         if (cell.column().address() === 1) {
